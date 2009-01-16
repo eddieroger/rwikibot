dd054d19807fd807eb9a214174bc93abda423476
Eddie Roger <eddieroger@gmail.com>
2009-01-16 12:54:37 -0600

    More meets problems...

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -4,7 +4,7 @@ module RWBUtilities
   def meets_version_requirement(maj, min)
     major, minor = @config['api_version'].to_s.split('.')
 #    puts "#{major} > #{maj}"
-    if ( major.to_i > maj.to_i ) || ( (major.to_i == maj.to_i) && (minor.to_i > min.to_i) )
+    if ( major.to_i > maj.to_i ) || ( (major.to_i == maj.to_i) && (minor.to_i >= min.to_i) )
       return true
     else 
       return false
